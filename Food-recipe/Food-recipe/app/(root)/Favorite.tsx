9b9933d79f1a36409a63e2c0655aff5bd7ce71c4
Nguyễn Thị Thu Sương <suongntt.22ite@vku.udn.vn>
2025-03-27 20:37:42 +0700

    Update code

---
+++ Food-recipe/Food-recipe/app/(root)/Favorite.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import Index from "../favorite/index"
+import React from "react";
+
+const Favorite = () => {
+  return (
+    <Index />  
+  );
+};
+
+export default Favorite;
---
+++ Food-recipe/Food-recipe/constant/colors.ts
@@ -0,0 +1,5 @@
+const colors = {
+  primary: "#0B9A61",
+  "text-primary": "#B0B0B0",
+};
+export default colors;
